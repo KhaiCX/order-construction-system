a89aef91a547b870eb1b3634dcbe9345e6c2cdca
KhaiCX <101654542+KhaiCX@users.noreply.github.com>
2024-04-18 10:54:33 +0700

    Create project

---
+++ src/main/java/com/vn/construction/orderconstructionsystem/OrderConstructionSystemApplication.java
@@ -0,0 +1,11 @@
+package com.vn.construction.orderconstructionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderConstructionSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConstructionSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/vn/construction/orderconstructionsystem/OrderConstructionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.construction.orderconstructionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderConstructionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
